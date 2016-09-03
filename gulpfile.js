cd6c7c993b1d0187fff69200ca03c8aa8377b3d3
Luis Perez <luisperez@college.harvard.edu>
2016-09-02 22:09:22 -0700

    improved build system

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp')
  , cleanCSS = require('gulp-clean-css')
  , rename = require("gulp-rename")
  , uglify = require('gulp-uglify')
+ , path = require("path")
  , pkg = require('./package.json');
 
 // Set the banner content
@@ -17,12 +18,14 @@ var banner = ['/*!\n',
     ''
 ].join('');
 
+var baseDir = path.join(__dirname, 'public');
+
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
     return gulp.src('less/creative.less')
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest(path.join(baseDir, "css")))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -33,7 +36,7 @@ gulp.task('minify-css', ['less'], function() {
     return gulp.src('css/creative.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest(path.join(baseDir, "css")))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -45,7 +48,7 @@ gulp.task('minify-js', function() {
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest(path.join(baseDir, "js")))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -53,17 +56,21 @@ gulp.task('minify-js', function() {
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
+    // copy images
+    gulp.src(['img/**'])
+        .pipe(gulp.dest(path.join(baseDir, "vendor", "bootstrap")))
+    // copy sources
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest(path.join(baseDir, "vendor", "bootstrap")))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest(path.join(baseDir, "vendor", "jquery")))
 
     gulp.src(['node_modules/magnific-popup/dist/*'])
-        .pipe(gulp.dest('vendor/magnific-popup'))
+        .pipe(gulp.dest(path.join(baseDir, "vendor", "magnific-popup")))
 
     gulp.src(['node_modules/scrollreveal/dist/*.js'])
-        .pipe(gulp.dest('vendor/scrollreveal'))
+        .pipe(gulp.dest(path.join(baseDir, "vendor", 'scrollreveal')))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -73,18 +80,18 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest(path.join(baseDir, "vendor", "font-awesome")))
 })
 
 // Our personal framework
 gulp.task('index', function() {
     return gulp.src([
-        'html/header.html',
+        'html/common/header.html',
         'html/index.html',
-        'html/footer.html'
+        'html/common/footer.html'
     ])
     .pipe(concat('index.html'))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest(baseDir));
 });
 
 // Build all the HTML pages.
@@ -97,7 +104,7 @@ gulp.task('default', ['build-html', 'less', 'minify-css', 'minify-js', 'copy']);
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
-            baseDir: ''
+            baseDir: baseDir
         },
     })
 })
