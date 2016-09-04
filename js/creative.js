28bf11ea2f871b5fd756eb2e8afa7c87965bac3e
Luis Perez <luisperez@college.harvard.edu>
2016-09-04 16:32:30 -0700

    construction page up

--- js/creative.js
+++ js/creative.js
@@ -1,7 +1,29 @@
 (function($) {
-    "use strict"; // Start of use strict
+  "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
+  /**
+   * Scrolls to the location on the page specified by the hash.
+   */
+  var scrollToHash = function(hash){
+    // We add '-content' to manually scroll into the div of our choice.
+    $('html, body').stop().animate({
+      scrollTop: ($(hash + "-content").offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+
+    // close the menu if open
+    $('.navbar-toggle:visible').click();
+  };
+
+  var scrollWindow = function(){
+    var hash = window.location.hash;
+    if (hash){
+      scrollToHash(hash);
+    }
+  };
+
+  // Wait until window has loaded!
+  $(window).load(function(){
+
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -10,64 +32,74 @@
         event.preventDefault();
     });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
+    // Scroll to the URL (ie, refresh location)
+    $('a.page-refresh').bind('click', function(event) {
+       var $anchor = $(this);
+       var hash = $anchor.attr('href');
+       if (hash){
+        scrollToHash(hash);
+      }
     });
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
+    // Blueberry slideshow!
+    $('#blueberry').waitForImages(function(){
+      $(this).blueberry({
+        pager: false,
+        duration: 5000
+      })
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+      target: '.navbar-fixed-top',
+      offset: 51
     });
 
     // Offset for Main Navigation
     $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
+      offset: {
+        top: 100
+      }
     })
 
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
     sr.reveal('.sr-icons', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
+      duration: 600,
+      scale: 0.3,
+      distance: '0px'
     }, 200);
     sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
+      duration: 1000,
+      delay: 200
     });
     sr.reveal('.sr-contact', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
+      duration: 600,
+      scale: 0.3,
+      distance: '0px'
     }, 300);
 
     // Initialize and Configure Magnific Popup Lightbox Plugin
     $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+          enabled: true,
+          navigateByImgClick: true,
+          preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+          tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+      }
     });
 
-    // Blueberry slideshow!
-    $(window).load(function() {
-            $('.blueberry').blueberry({
-                pager: false,
-                keynav: false,
-                duration: 1000
-            });
-        });
+    // scroll to location
+    scrollWindow();
+
+    // Track hash changes and scroll to location
+    $(window).on('hashchange', scrollWindow);
+  });
 
 })(jQuery); // End of use strict
